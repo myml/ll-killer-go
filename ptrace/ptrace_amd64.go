085ae59857bab6a851657afbaa98d7b2a71212e1
System233 <20336040+System233@users.noreply.github.com>
2025-03-02 13:15:05 +0800

    fix: fchownat/lchown系统调用

--- ptrace/ptrace_amd64.go
+++ ptrace/ptrace_amd64.go
@@ -19,10 +19,20 @@ import (
 var IsSupported = true
 
 func PtraceHandle(pid int, regs unix.PtraceRegs) error {
-	if regs.Orig_rax == syscall.SYS_CHOWN || regs.Orig_rax == syscall.SYS_FCHOWN || regs.Orig_rax == syscall.SYS_FCHOWNAT {
+	if regs.Orig_rax == syscall.SYS_CHOWN || regs.Orig_rax == syscall.SYS_FCHOWN || regs.Orig_rax == syscall.SYS_LCHOWN {
 		if os.Getuid() != int(regs.Rdx) || os.Getgid() != int(regs.R10) {
 			regs.Rdx = uint64(os.Getuid())
 			regs.R10 = uint64(os.Getgid())
+			err := unix.PtraceSetRegs(pid, &regs)
+			if err != nil {
+				return fmt.Errorf("PtraceSetRegs:%v", err)
+			}
+		}
+	}
+	if regs.Orig_rax == syscall.SYS_FCHOWNAT {
+		if os.Getuid() != int(regs.R10) || os.Getgid() != int(regs.R8) {
+			regs.R10 = uint64(os.Getuid())
+			regs.R8 = uint64(os.Getgid())
 			err := unix.PtraceSetRegs(pid, &regs)
 			if err != nil {
 				return fmt.Errorf("PtraceSetRegs:%v", err)
--- ptrace/ptrace_arm64.go
+++ ptrace/ptrace_arm64.go
@@ -19,10 +19,20 @@ import (
 var IsSupported = true
 
 func PtraceHandle(pid int, regs unix.PtraceRegs) error {
-	if regs.Regs[8] == syscall.SYS_FCHOWN || regs.Regs[8] == syscall.SYS_FCHOWNAT {
+	if regs.Regs[8] == syscall.SYS_FCHOWN {
 		if os.Getuid() != int(regs.Regs[2]) || os.Getgid() != int(regs.Regs[3]) {
 			regs.Regs[2] = uint64(os.Getuid())
 			regs.Regs[3] = uint64(os.Getgid())
+			err := unix.PtraceSetRegs(pid, &regs)
+			if err != nil {
+				return fmt.Errorf("PtraceSetRegs:%v", err)
+			}
+		}
+	}
+	if regs.Regs[8] == syscall.SYS_FCHOWNAT {
+		if os.Getuid() != int(regs.Regs[3]) || os.Getgid() != int(regs.Regs[4]) {
+			regs.Regs[3] = uint64(os.Getuid())
+			regs.Regs[4] = uint64(os.Getgid())
 			err := unix.PtraceSetRegs(pid, &regs)
 			if err != nil {
 				return fmt.Errorf("PtraceSetRegs:%v", err)
