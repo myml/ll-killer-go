4e222339b5f05ae5985162e555af1a686825dd31
System233 <20336040+System233@users.noreply.github.com>
2025-03-03 12:58:29 +0800

    fix: 适配1.7.x

--- build-aux/env.sh
+++ build-aux/env.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 if [ -z "$ENV_SETUPED" ];then
     export ENV_SETUPED=1
+    export PATH=$PATH:$(dirname $0):$PWD
     export ENTRYPOINT_NAME=${ENTRYPOINT_NAME:-entrypoint.sh}
     export ENTRYPOINT=${ENTRYPOINT:-/opt/apps/$LINGLONG_APPID/files/$ENTRYPOINT_NAME}
     export KILLER_EXEC=${KILLER_EXEC:-$(which ll-killer)}
-    export PATH=$PATH:$(dirname $0)
     
     if [ -z "$KILLER_EXEC" ];then
-        echo "错误：未找到ll-killer，请确保ll-killer在PATH环境变量中，或使用'll-killer script $*'重新执行命令。" >&2
+        echo "错误：未找到ll-killer，请确保ll-killer在当前或build-aux目录中，或使用'll-killer script $*'重新执行命令。" >&2
         exit 1
     fi
 fi
