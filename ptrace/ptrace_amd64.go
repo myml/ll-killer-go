f57c517208ee98fcfa6878a926138d675ae7b417
System233 <20336040+System233@users.noreply.github.com>
2025-03-04 19:33:24 +0800

    fix: 更正amd64系统调用寄存器

--- ptrace/ptrace_amd64.go
+++ ptrace/ptrace_amd64.go
@@ -20,16 +20,15 @@ var IsSupported = true
 
 func PtraceHandle(pid int, regs unix.PtraceRegs) error {
 	if regs.Orig_rax == syscall.SYS_CHOWN || regs.Orig_rax == syscall.SYS_FCHOWN || regs.Orig_rax == syscall.SYS_LCHOWN {
-		if os.Getuid() != int(regs.Rsi) || os.Getgid() != int(regs.Rdi) {
+		if os.Getuid() != int(regs.Rsi) || os.Getgid() != int(regs.Rdx) {
 			regs.Rsi = ^uint64(0)
-			regs.Rdi = ^uint64(0)
+			regs.Rdx = ^uint64(0)
 			err := unix.PtraceSetRegs(pid, &regs)
 			if err != nil {
 				return fmt.Errorf("PtraceSetRegs:%v", err)
 			}
 		}
-	}
-	if regs.Orig_rax == syscall.SYS_FCHOWNAT {
+	} else if regs.Orig_rax == syscall.SYS_FCHOWNAT {
 		if os.Getuid() != int(regs.Rdx) || os.Getgid() != int(regs.R10) {
 			regs.Rdx = ^uint64(0)
 			regs.R10 = ^uint64(0)
